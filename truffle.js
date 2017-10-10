051238eb87b48f1d0d4451702709321e731cac10
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2017-10-10 02:13:21 -0300

    Keeping the migrations

--- truffle.js
+++ truffle.js
@@ -4,6 +4,15 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8546,
+      network_id: "4" // Match any network id
     }
+  },
+  rpc: {
+    host: "localhost",
+    port: 8545
   }
 };
